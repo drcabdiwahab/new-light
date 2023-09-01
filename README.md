aa0739755ff8f0b5050d0c8fa8feb7c753ba894d
drcabdiwahab <dr.cabdiwahab@gmail.com>
2023-09-01 03:52:39 -0600

    third commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Added README.md
+Removed also all comments
+https://drcabdiwahab.github.io/new-light/ LIVE IN GITHUB PAGES
+ADDING FULL RESPONSIVENESS
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const x = document.querySelector(".x")
 
 x && (x.onclick = () => document.querySelector("nav").classList.toggle("db"))
 
-document.querySelectorAll("a").forEach(a =>a.onclick = e => {
+document.querySelectorAll("a").forEach(a => a.onclick = e => {
   e.preventDefault()
-  sessionStorage.setItem("price", e.target.parentElement.querySelector("p").textContent)
+  sessionStorage.setItem("price", e.target?.parentElement.querySelector("p").textContent)
   location.href = e.target.parentElement.getAttribute("href")
 })
 
-if (sessionStorage.getItem("price")) {
-  const [discount, price] = sessionStorage.getItem("price").split(" ")
-  document.querySelector(".buy p + p").innerHTML = `${price} <s>${discount}</s>`;
+const p = document.querySelector(".buy p + p")
+const prce = sessionStorage.getItem("price")
+if (prce && p) {
+  const [discount, price] = prce.split(" ")
+  p.innerHTML = `${price} <s>${discount}</s>`;
 }
